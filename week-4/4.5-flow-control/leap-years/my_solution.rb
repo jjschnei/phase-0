5298b8d747deeb81067b3b70466a451d16f41674
Jeff Schneider <jeffjschneider@gmail.com>
2015-12-03 14:22:59 -0800

    leap year

--- week-4/4.5-flow-control/leap-years/my_solution.rb
+++ week-4/4.5-flow-control/leap-years/my_solution.rb
@@ -4,3 +4,13 @@
 
 
 # Your Solution Below
+def leap_year?(year)
+   if (year % 4 == 0) && ((year % 100 == 0) && (year % 400 == 0))
+      return true
+   elsif (year % 4 == 0) && (year % 100 != 0)
+      return true
+    else
+      false
+    end
+  end
+
