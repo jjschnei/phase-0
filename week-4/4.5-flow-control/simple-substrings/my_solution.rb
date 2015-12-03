0b5ae783fead097942f46b94e194bfb3630a712f
Jeff Schneider <jeffjschneider@gmail.com>
2015-12-03 08:57:12 -0800

    prep for 4.5 and gps1.2

---
+++ week-4/4.5-flow-control/simple-substrings/my_solution.rb
@@ -0,0 +1,14 @@
+# Simple Substrings
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+def welcome(user_address)
+  if user_address.include? "CA"
+    return "Welcome to California"
+  else
+    return "You should move to California"
+  end
+end
+
