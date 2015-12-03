26a78112b97934231bc7d26f1b709b89e9238925
Jeff Schneider <jeffjschneider@gmail.com>
2015-12-03 13:23:13 -0800

    create 4.5.2

--- week-4/4.5-flow-control/calculate-grade/my_solution.rb
+++ week-4/4.5-flow-control/calculate-grade/my_solution.rb
@@ -4,3 +4,21 @@
 
 
 # Your Solution Below
+
+def get_grade (average)
+  if average < 60
+    return "F"
+  elsif average < 70
+    return "D"
+  elsif average < 80
+    return "C"
+  elsif average < 90
+    return "B"
+  else
+    return "A"
+  end
+end
+
+
+
+
