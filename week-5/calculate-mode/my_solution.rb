80c8897dba988b5f438effb542c22f7023cd18ba
Jeff Schneider <jeffjschneider@gmail.com>
2015-12-10 11:14:29 -0800

    finish 5.3

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -13,17 +13,56 @@
 # 0. Pseudocode
 
 # What is the input?
+#take an array of numbers or strings
 # What is the output? (i.e. What should the code return?)
+#Array of the most frequent values
+
 # What are the steps needed to solve the problem?
+# Iterate through the array
+# Turn the array into a hash, each item from the array will be a key. The frequency of that key will be the value.
+#Push the key of the highest value to a new array
+#return new array
 
 
 # 1. Initial Solution
+def mode(array)
+  mode_hash = Hash.new
+  array.each do |x|
+    if mode_hash.has_key?(x)
+      mode_hash[x] += 1
+    else
+      mode_hash[x] = 1
+    end
+  end
+  a = mode_hash.values.sort.last
+  answer_array = []
+  mode_hash.each do |key, value|
+    if value == a
+      answer_array << key
+    end
+  end
 
+  return answer_array
 
+end
 
 
 # 3. Refactored Solution
-
+def mode(array)
+  mode_hash = Hash.new
+  array.each do |x|
+    if mode_hash.has_key?(x)
+      mode_hash[x] += 1
+    else
+      mode_hash[x] = 1
+    end
+  end
+  a = mode_hash.values.sort.last
+  answer_array = []
+  x = mode_hash.select { |key, value| value == a}
+  answer_array = x.keys
+  return answer_array
+end
 
 
 
