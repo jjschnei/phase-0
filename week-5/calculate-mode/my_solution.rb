ef0e0003d77ee3847dc1037dd557a50689d80f12
Jeff Schneider <jeffjschneider@gmail.com>
2015-12-10 11:18:45 -0800

    made reflection to 5.3

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -67,3 +67,15 @@ end
 
 
 # 4. Reflection
+
+# Which data structure did you and your pair decide to implement and why?
+#We decided to use a hash.  We saw the hint in the instructions and then discussed how we would use it.
+
+# Were you more successful breaking this problem down into implementable pseudocode than the last with a pair?
+#No, it was the same.  Breaking it down has proven to be the easy part.  It's figuring out how to actually code the pseudocode that is difficutl.
+
+# What issues/successes did you run into when translating your pseudocode to code?
+#We didn't know the methods we needed so we had to do research to figure them out.
+
+# What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+#we used each and if. During refactoring we learned how to use select. This made the code a bit tighter.
