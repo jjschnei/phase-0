144e3c1b85b59585157d9289b69148a844c51b26
Jeff Schneider <jeffjschneider@gmail.com>
2016-01-02 18:30:55 -0800

    person 2 for 7.8

---
+++ week-7/group_project_solutions.js
@@ -0,0 +1,44 @@
+Release 1: Tests to User Stories
+
+As a user, I want to calculate the sum of an array.
+
+pseudocode:
+// Accept an array of numbers from the user.
+// Create a new variable to hold the sum of the array.  Set it to = 0
+// Iterate through the user's array. You can do this with a FOR loop.
+// The FOR loop should have a counter starting at 0. It should loop for the length of the users array. It should add 1 to the coutner for each loop.
+// The code block in the loop should add each number to the new variable you've created. Use the counter as the index number in the user's array.
+// When the loop is finished, the new variable that you created will equal the sum of the user's array.
+
+
+As a user, I want to calculate the mean of an array.
+
+pseudocode:
+// Accept an array of numbers from the user.
+// Create a new variable to hold the sum of the array.  Set it to = 0
+// Iterate through the user's array. You can do this with a FOR loop.
+// The FOR loop should have a counter starting at 0. It should loop for the length of the users array. It should add 1 to the coutner for each loop.
+// The code block in the loop should add each number to the new variable you've created. Use the counter as the index number in the user's array.
+// When the loop is finished, the new variable that you created will equal the sum of the user's array.
+// Divide the new variable contaiing the sum by the length of the user's array.  This will give the mean of the array.
+
+As a user, I want to calculate the median of an odd length array.
+
+pseudocode:
+// Accept an array of numbers from the user.
+// Order the array by the size of the elements. (Reminder: you will neet to use the compareFunction to sort numbers by size)
+// Create a new variable to hold the value of the median.
+// Set the new variable to the ordered array with an index of the array.length minus 1 and divided by 2.
+// The new variable is now equal to the median of the array.
+
+
+
+As a user, I want to calculate the median of an even length array.
+
+pseudocode:
+// Accept an array of numbers from the user.
+// Order the array by the size of the elements. (Reminder: you will neet to use the compareFunction to sort numbers by size)
+// Create two new variables.  One will be for the lower of the two middle numbers and one will be for the higher of the two middle numbers.
+// Set the lower variable to the ordered array with an index of the array.length divided by 2.
+// Set the higher variable to the ordered array with the result of index of the array.length divided by 2 and then plus 1.
+// Create a new variable called median.  Set it equal to the sum of the lower variable and higher variable divded by 2.  Be careful with your order of opperations.  You will need brackets around the addition so that it happens before the division.
